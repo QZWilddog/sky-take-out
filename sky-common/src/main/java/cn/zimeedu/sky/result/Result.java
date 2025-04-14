3dc3023f716b538cb671bbc311bc8a9677bcf5c6
haiqiu <2477116273@qq.com>
2025-04-14 23:36:20 +0800

    完成苍穹外卖登录部分不完善

---
+++ sky-common/src/main/java/cn/zimeedu/sky/result/Result.java
@@ -0,0 +1,35 @@
+package cn.zimeedu.sky.result;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * 后端统一返回结果
+ * 这种设计模式被称为“静态工厂方法模式”  封装对象创建过程：  提高可读性：  灵活性：  隐藏实现细节：
+ */
+@Data
+public class Result<T> implements Serializable {
+    private Integer code;  //编码：1成功，0和其它数字为失败
+    private T data;  //数据
+    private String msg;   //错误信息
+
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result result = new Result();
+        result.msg = msg;
+        result.code = 0;
+        return result;
+    }
+}
